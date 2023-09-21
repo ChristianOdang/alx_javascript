6bc91ad4630b64f8c5314a0e610629a4aa567f2f
JohnsonOdang <johnnyodanny@gmail.com>
2023-09-21 01:08:24 -0700

    setting cookies

--- Cookie_local_storage/src/index.js
+++ Cookie_local_storage/src/index.js
@@ -1,11 +1,11 @@
 function setCookies() {
-  const firstName = document.getElementById("firstname").value;
+  const firstname = document.getElementById("firstname").value;
   const eMail = document.getElementById("email").value;
   const date = new Date();
   date.setDate(date.getDate() + 10);
   const expires = "expires=" + date.toDateString();
-  if (firstName) {
-    document.cookie = `firstname= ${firstName}; ${expires}; path=/`;
+  if (firstname) {
+    document.cookie = `firstname= ${firstname}; ${expires}; path=/`;
   }
   if (eMail) {
     document.cookie = `email= ${eMail}; ${expires}; path=/`;
@@ -15,6 +15,19 @@ function setCookies() {
 function showCookies() {
   const show_ckies = document.createElement("P");
   show_ckies.innerHTML = `Cookies: ${document.cookie}`;
+  const EMAIL = getCookies("email");
+  const FirstName = getCookies("firstname");
+  show_ckies.innerHTML = `Email: ${EMAIL} - Firstname: ${FirstName}`;
   const sh_body = document.querySelector("body");
   sh_body.append(show_ckies);
+}
+
+function getCookies(name) {
+  const decodeCookie = decodeURIComponent(document.cookie);
+  const list = decodeCookie.split(";");
+  for (let i of list) {
+    let cookie = i.split("=");
+    if (name === cookie[0].trim()) return cookie[1].trim();
+  }
+  return "";
 }
